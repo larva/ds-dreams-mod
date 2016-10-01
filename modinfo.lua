9e2fb246f54f03fd4bfc35fca22d4933abd9d711
Matt Helsley <matt.helsley@gmail.com>
2016-09-30 20:23:30 -0700

    Bump module version to reflect trimmage
    Signed-off-by: Matt Helsley <matt.helsley@gmail.com>

--- modinfo.lua
+++ modinfo.lua
@@ -1,5 +1,5 @@
 -- http://dont-starve-modding.wikia.com/wiki/Modinfo.lua 
-version = "0.5"
+version = "0.6"
 name = "Dream Snatcher"
 description = "Harnessing the stuff of nightmares. Haunt the dreams of your friends for fun and profit!"
 author = "larva"
