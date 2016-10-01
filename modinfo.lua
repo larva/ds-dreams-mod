7c185a823d3332f6adbca4b645ad0c7cc837f3eb
Matt Helsley <matt.helsley@gmail.com>
2016-10-01 10:21:39 -0700

    Set API versions and compat stuff for DST
    Signed-off-by: Matt Helsley <matt.helsley@gmail.com>

--- modinfo.lua
+++ modinfo.lua
@@ -1,5 +1,5 @@
 -- http://dont-starve-modding.wikia.com/wiki/Modinfo.lua 
-version = "0.61"
+version = "0.71"
 name = "Dream Snatcher"
 description = "Harnessing the stuff of nightmares. Haunt the dreams of your friends for fun and profit!"
 author = "larva"
@@ -11,21 +11,23 @@ dont_starve_compatible = true
 reign_of_giants_compatible = true
 shipwrecked_compatible = true
 
-
 --- TODO DST compat:
 --- http://forums.kleientertainment.com/topic/47353-guide-getting-started-with-modding-dst-and-some-general-tips-for-ds-as-well/
---api_version_dst = 10
---dst_compatible = true
---server_filter_tags = { "dreamsnatcher", "mod adds a structure" }
+api_version_dst = 10
+dst_compatible = true
+server_filter_tags = {}
 
 --- This lets clients know if they need to get the mod from the Steam Workshop
 --- to join the game
---all_clients_require_mod = true
+all_clients_require_mod = true
 
 --- This determines whether it causes a server to be marked as modded (and shows
 --- in the mod list)
---client_only_mod = false
+clients_only_mod = false
+
 --- END TODO DST compat
 
 icon_atlas = "modicon.xml"
 icon = "modicon.tex"
+
+configuration_options = {}
